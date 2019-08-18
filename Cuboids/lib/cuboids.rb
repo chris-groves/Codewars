51be4dcb1aafcd8b69d6a86f578511e35597429a
Chris Groves <chris_groves87@hotmail.co.uk>
2019-08-18 14:41:28 +0000

    test: calculate differece of sum of arrays where sum of first array is larger

--- Cuboids/lib/cuboids.rb
+++ Cuboids/lib/cuboids.rb
@@ -2,6 +2,6 @@ class Cuboids
     def find_difference(a, b)
         a_total = a.sum
         b_total = b.sum
-        a_total + b_total
+        a_total - b_total
     end
 end
--- Cuboids/spec/cuboids_spec.rb
+++ Cuboids/spec/cuboids_spec.rb
@@ -2,8 +2,8 @@ require 'cuboids'
 
 describe Cuboids do
     it 'calculates the total of 2 arrays and adds the totals together' do
-        a = [1,2,3]
-        b = [3,4,5]
-        expect(subject.find_difference(a, b)).to eq 18
+        a = [3,4,5]
+        b = [1,2,3]
+        expect(subject.find_difference(a, b)).to eq 6
     end
 end
