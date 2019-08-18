209c24a0e6bd27381e737a60aef6c80cb72f8687
Chris Groves <chris_groves87@hotmail.co.uk>
2019-08-18 15:05:37 +0000

    fix: return to first test following new understanding of problem

--- Cuboids/lib/cuboids.rb
+++ Cuboids/lib/cuboids.rb
@@ -1,7 +1,5 @@
 class Cuboids
-    def find_difference(a, b)
-        a_total = a.sum
-        b_total = b.sum
-        a_total - b_total
+    def find_difference(a)
+        a.inject(:*)
     end
 end
--- Cuboids/spec/cuboids_spec.rb
+++ Cuboids/spec/cuboids_spec.rb
@@ -1,9 +1,14 @@
 require 'cuboids'
 
 describe Cuboids do
-    it 'calculates the total of 2 arrays and adds the totals together' do
-        a = [3,4,5]
-        b = [1,2,3]
-        expect(subject.find_difference(a, b)).to eq 6
+    it 'multiplies the elements of an array' do
+        a = [1,2,3]
+        expect(subject.find_difference(a)).to eq 6
     end
+    
+  #  it 'multiplies the elements of 2 arrays and finds the difference where the second array total is larger' do
+  #      a = [1,2,3]
+   #     b = [3,4,5]
+    #    expect(subject.find_difference(a, b)).to eq 6
+  #  end    
 end
